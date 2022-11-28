abe35de7db2d8013c2bd8a4ef1c221b037eb9be6
makerseok <maker7788@gmail.com>
2022-11-28 23:25:16 +0900

    change db schema

--- js/db.js
+++ js/db.js
@@ -1,10 +1,9 @@
 const dbName = 'ReportsDB';
-const dbVersion = 2;
+const dbVersion = 3;
 
 let db = new Dexie(dbName);
 
 db.version(dbVersion).stores({
-  reports:
-    '++id,COMPANY_ID,DEVICE_ID,FILE_ID,HIVESTACK_YN,HIVESTACK_URL,PLAY_ON',
+  reports: '++,PLAY_ON',
   caches: '++id,cachedOn',
 });
