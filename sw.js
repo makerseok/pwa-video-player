65f83449bff9217cae90febd388e3bba577f198c
makerseok <maker7788@gmail.com>
2022-12-19 16:59:34 +0900

    sw 안정성 및 form Enter 입력 시 동작 개선

--- sw.js
+++ sw.js
@@ -88,7 +88,7 @@ self.addEventListener('fetch', event => {
   // console.log('fetch event', event);
   const scope = self.registration.scope;
   const url = new URL(event.request.url);
-  const queryReplacedUrl = url.href.replace(url.search, '');
+  const queryReplacedUrl = url.href.replace(url.search, '').split('?')[0];
   if (
     [scope, scope + 'index.html', scope + 'index.htm'].includes(
       queryReplacedUrl,
